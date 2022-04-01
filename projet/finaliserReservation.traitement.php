4b8053922d2fa6f67b03e68c038b0ca23b279bad
Clem-0000 <90420417+Clem-0000@users.noreply.github.com>
2022-04-01 09:33:35 +0200

    Delete projet directory

--- projet/finaliserReservation.traitement.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include_once 'gestion.base.inc.php';
-
-$codeReservation = $_SESSION["codeReservation"];
-foreach ($_SESSION["ligneDeReservation"] as $key => $value) {
-    ajouterLigneDeReservation($codeReservation, $key, $value);
-}
-header("location:reservation.php");
